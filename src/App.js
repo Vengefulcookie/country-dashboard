421fc0ae3697ea64cd5661730010cf0e79dd0d7d
Sne Shangase <snethemba.nosipho.shangase@gmail.com>
2026-04-21 21:02:34 +0200

    Added a footer

--- src/App.js
+++ src/App.js
@@ -8,6 +8,9 @@ function App() {
                 <span>Discover flags, capitals, populations & more! 🎈</span>
             </div>
             <CountrySearch />
+            <footer className="project-footer">
+                <p>This AI project was coded by Snethemba Shangase 🖤 and is on <a href="https://github.com/Vengefulcookie/country-dashboard" target="_blank" rel="noopener noreferrer"> GitHub </a> and hosted on <a href="https://vcookie-country-dashboard.netlify.app/" target="_blank" rel="noopener noreferrer"> Netlify</a>.</p>
+            </footer>
         </div>
     );
 }
